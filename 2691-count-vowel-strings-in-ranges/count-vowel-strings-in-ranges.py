4e5a8e6567dc6769591e554314b6d211ad8a9447
Raf-codes <163875268+Raf-codes@users.noreply.github.com>
2025-01-02 19:45:25 +0600

    Time: 25 ms (32.69%) | Memory: 51.8 MB (6.67%) - LeetSync

--- 2691-count-vowel-strings-in-ranges/count-vowel-strings-in-ranges.py
+++ 2691-count-vowel-strings-in-ranges/count-vowel-strings-in-ranges.py
@@ -1,14 +1,15 @@
 class Solution:
-   def valid(self, s: str) -> bool:
-       vowels = set('aeiou')
-       return s[0] in vowels and s[-1] in vowels
+   def isVowel(self, c: str) -> bool:
+       return c in 'aeiou'
        
    def vowelStrings(self, words: List[str], queries: List[List[int]]) -> List[int]:
-       n = len(words)
-       prefix = [0] * n
-       prefix[0] = 1 if self.valid(words[0]) else 0
+       # Mark words starting and ending with vowels
+       v = [1 if self.isVowel(word[0]) and self.isVowel(word[-1]) else 0 for word in words]
        
-       for i in range(1, n):
-           prefix[i] = prefix[i-1] + (1 if self.valid(words[i]) else 0)
+       # Build prefix sum array
+       prefix = [v[0]] + [0] * (len(v)-1)
+       for i in range(1, len(v)):
+           prefix[i] = prefix[i-1] + v[i]
            
-       return [prefix[q[1]] if q[0] == 0 else prefix[q[1]] - prefix[q[0]-1] for q in queries]
+       # Process queries
+       return [prefix[r] if l == 0 else prefix[r] - prefix[l-1] for l, r in queries]
