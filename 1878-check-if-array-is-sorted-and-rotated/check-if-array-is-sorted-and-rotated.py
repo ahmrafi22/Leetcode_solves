fdd6ee6d69a0e613390db96a529e9dd278fda7e6
Raf-codes <163875268+Raf-codes@users.noreply.github.com>
2025-02-02 21:28:26 +0600

    Time: 0 ms (100.00%) | Memory: 17.7 MB (47.79%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.py
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.py
@@ -1,6 +1,3 @@
 class Solution:
     def check(self, nums: List[int]) -> bool:
-
-        rotations = sum(nums[i] < nums[i-1] for i in range(len(nums)))
-
-        return rotations <= 1
+        return sum(nums[i] < nums[i-1] for i in range(len(nums))) <= 1
