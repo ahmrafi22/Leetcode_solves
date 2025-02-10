5a3d7ff28329141d0e86a4694c531e377e07f135
Raf-codes <163875268+Raf-codes@users.noreply.github.com>
2025-02-10 20:23:20 +0600

    Time: 1 ms (41.40%) | Memory: 17.7 MB (70.70%) - LeetSync

--- 3447-clear-digits/clear-digits.py
+++ 3447-clear-digits/clear-digits.py
@@ -1,11 +1,14 @@
 class Solution:
     def clearDigits(self, s: str) -> str:
-        stack=[]
-        for each in s:
-            if each.isdigit():
-                if stack:
-                    stack.pop()
-            else:
-                stack.append(each)
-        return "".join(stack)
+        chars = list(s)
+        marked = [False] * len(chars)
         
+        for i in range(len(chars)):
+            if chars[i].isdigit():
+                for j in range(i-1, -1, -1):
+                    if not marked[j] and not chars[j].isdigit():
+                        marked[j] = True
+                        break
+        
+        return ''.join(char for i, char in enumerate(chars) 
+                       if not marked[i] and not char.isdigit())
