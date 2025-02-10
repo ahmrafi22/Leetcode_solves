6f2c321c110b94c380824fdd5407456c7ddca304
Raf-codes <163875268+Raf-codes@users.noreply.github.com>
2025-02-10 20:23:47 +0600

    Time: 0 ms (100.00%) | Memory: 17.9 MB (27.31%) - LeetSync

--- 3447-clear-digits/clear-digits.py
+++ 3447-clear-digits/clear-digits.py
@@ -1,14 +1,9 @@
 class Solution:
-    def clearDigits(self, s: str) -> str:
-        chars = list(s)
-        marked = [False] * len(chars)
-        
-        for i in range(len(chars)):
-            if chars[i].isdigit():
-                for j in range(i-1, -1, -1):
-                    if not marked[j] and not chars[j].isdigit():
-                        marked[j] = True
-                        break
-        
-        return ''.join(char for i, char in enumerate(chars) 
-                       if not marked[i] and not char.isdigit())
+   def clearDigits(self, s: str) -> str:
+       result = []
+       for char in s:
+           if char.isdigit():
+               result.pop() if result else None
+           else:
+               result.append(char)
+       return ''.join(result)
