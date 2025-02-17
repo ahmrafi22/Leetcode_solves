344957913a1cc10a3c3e01aedcff070ff39f55a4
Raf-codes <163875268+Raf-codes@users.noreply.github.com>
2025-02-17 20:58:43 +0600

    Time: 19 ms (88.75%) | Memory: 17.7 MB (83.06%) - LeetSync

--- 1160-letter-tile-possibilities/letter-tile-possibilities.py
+++ 1160-letter-tile-possibilities/letter-tile-possibilities.py
@@ -1,3 +1,16 @@
 class Solution:
     def numTilePossibilities(self, tiles: str) -> int:
-        return len(set(p for i in range(1, len(tiles)+1) for p in permutations(tiles, i)))
+        freq = {}
+        for c in tiles:
+            freq[c] = freq.get(c, 0) + 1
+        
+        def backtrack(curr_freq):
+            total = 0
+            for char in curr_freq:
+                if curr_freq[char] > 0:
+                    curr_freq[char] -= 1
+                    total += 1 + backtrack(curr_freq)
+                    curr_freq[char] += 1
+            return total
+            
+        return backtrack(freq)
