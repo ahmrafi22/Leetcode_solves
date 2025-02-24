0731bd447988ebb67cdf9fcdbe58123dcd609f73
Raf-codes <163875268+Raf-codes@users.noreply.github.com>
2025-02-24 20:08:05 +0600

    Time: 183 ms (95.59%) | Memory: 60.1 MB (72.88%) - LeetSync

--- 2564-most-profitable-path-in-a-tree/most-profitable-path-in-a-tree.py
+++ 2564-most-profitable-path-in-a-tree/most-profitable-path-in-a-tree.py
@@ -1,32 +1,44 @@
 class Solution:
-    def mostProfitablePath(self, edges: List[List[int]], bob: int, amount: List[int]) -> int:
-        graph = {i: [] for i in range(len(amount))}
+    def mostProfitablePath(self, edges, bob, amount):
+        n = len(amount)
+        graph = [[] for _ in range(n)]
         for u, v in edges:
             graph[u].append(v)
             graph[v].append(u)
         
-        bobPath = [-1] * len(amount)
-        path = []
-
-        def fillBobPath(node, parent):
+        bob_time_to_node = [-1] * n
+        
+        def find_bob_path(node, parent, time):
             if node == 0:
+                bob_time_to_node[node] = time
                 return True
+                
             for neighbor in graph[node]:
                 if neighbor != parent:
-                    path.append(node)
-                    if fillBobPath(neighbor, node):
+                    if find_bob_path(neighbor, node, time + 1):
+                        bob_time_to_node[node] = time
                         return True
-                    path.pop()
-
-        fillBobPath(bob, -1)
-        for i, node in enumerate(path):
-            bobPath[node] = i
+            return False
         
-        def getAliceMaxScore(node, parent, currScore, timestamp):
-            if bobPath[node] == -1 or bobPath[node] > timestamp:
-                currScore += amount[node]
-            elif bobPath[node] == timestamp:
-                currScore += amount[node] // 2
-            return currScore if len(graph[node]) == 1 and node != 0 else max(getAliceMaxScore(neighbor, node, currScore, timestamp + 1) for neighbor in graph[node] if neighbor != parent)
-
-        return getAliceMaxScore(0, -1, 0, 0)
+        find_bob_path(bob, -1, 0)
+        
+        def alice_dfs(node, parent, time, score):
+            curr_score = score
+            
+            if bob_time_to_node[node] == -1 or time < bob_time_to_node[node]:
+                curr_score += amount[node]
+            elif time == bob_time_to_node[node]:
+                curr_score += amount[node] // 2
+            
+            is_leaf = node != 0 and len(graph[node]) == 1
+            if is_leaf:
+                return curr_score
+            
+            max_score = float('-inf')
+            for neighbor in graph[node]:
+                if neighbor != parent:
+                    max_score = max(max_score, alice_dfs(neighbor, node, time + 1, curr_score))
+            
+            return max_score
+        
+        return alice_dfs(0, -1, 0, 0)
